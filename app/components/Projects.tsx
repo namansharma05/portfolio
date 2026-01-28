ff3ef63a4b76bba1c0e62811537f771573ce8c66
namansharma05 <namanks0511@gmail.com>
2026-01-28 21:40:02 +0530

    changed project names and description

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -6,19 +6,19 @@ export const Projects = () => {
       "Sorting Visualizer is an interactive website that demonstrates Merge Sort, Quick Sort, and Selection Sort through real-time animations. Users can adjust the array size, control the visualization speed, and generate a new unsorted array to understand each algorithm step by step.",
     ],
     [
-      "Sorting Visualizer",
-      "https://github.com/namansharma05/Sorting-Visualizer",
-      "Sorting Visualizer is an interactive website that demonstrates Merge Sort, Quick Sort, and Selection Sort through real-time animations. Users can adjust the array size, control the visualization speed, and generate a new unsorted array to understand each algorithm step by step.",
+      "Counter dApp",
+      "https://github.com/namansharma05/counter-dapp",
+      "This is a simple dApp for learning purpose only, where I have tried to simulate a functions like Initialize counter, Increment counter, Decrement counter, Closing counter.",
     ],
     [
-      "Sorting Visualizer",
-      "https://github.com/namansharma05/Sorting-Visualizer",
-      "Sorting Visualizer is an interactive website that demonstrates Merge Sort, Quick Sort, and Selection Sort through real-time animations. Users can adjust the array size, control the visualization speed, and generate a new unsorted array to understand each algorithm step by step.",
+      "Escrow dApp",
+      "https://github.com/namansharma05/escrow-dapp",
+      "A full-stack Solana escrow marketplace built with Anchor, React, Next.js, and Tailwind CSS. Users can mint SED tokens (admin-only) and buy tokens with SOL via escrow.",
     ],
     [
-      "Sorting Visualizer",
-      "https://github.com/namansharma05/Sorting-Visualizer",
-      "Sorting Visualizer is an interactive website that demonstrates Merge Sort, Quick Sort, and Selection Sort through real-time animations. Users can adjust the array size, control the visualization speed, and generate a new unsorted array to understand each algorithm step by step.",
+      "Bidding dApp",
+      "https://github.com/namansharma05/bidding-dapp",
+      "Bidding dApp is a decentralized application built on the Solana blockchain that allows users to list items for auction and accept bids within a fixed time duration. Participants can place bids until the auction ends, after which the highest bidder receives ownership authority of the item. The winning bid amount is securely transferred to the previous owner through an escrow account, ensuring trust-less and transparent fund handling.",
     ],
   ];
   return (
@@ -30,7 +30,7 @@ export const Projects = () => {
         {projects.map((project, index) => (
           <div
             key={index}
-            className="flex flex-col items-center gap-2 group cursor-default p-3 bg-gray-100 dark:bg-zinc-800/50 rounded-2xl transition-all duration-300 group-hover:scale-110 group-hover:bg-zinc-200 dark:hover:bg-zinc-700"
+            className="flex flex-col items-center gap-2 group cursor-default p-3 bg-gray-100 dark:bg-zinc-800/50 rounded-2xl transition-all duration-300 hover:scale-110 hover:bg-zinc-200 dark:hover:bg-zinc-700"
           >
             <div className="">
               <img
